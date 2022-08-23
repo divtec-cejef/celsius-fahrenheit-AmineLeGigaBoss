61fb02f785c26fb529d897c9f37b0bcd72838c13
sebamoh <mohammedamine.sebaoun@divtec.ch>
2022-08-23 15:57:33 +0200

    finito

--- js/main.js
+++ js/main.js
@@ -6,10 +6,17 @@
  */
 
 (function main() {
-  'use strict'; // Demande un interprétation stricte du code
+    'use strict'; // Demande un interprétation stricte du code
 
-  let a = 'Bonjour';
+    let a = 'Bonjour';
 
-  console.log(a);
+    let celsius;
+    do {
+      celsius = prompt('Température en celsius :');
+    } while (isNaN(celsius));
+
+    alert(`${celsius}°C = ${(celsius * 9 / 5 + 32).toFixed(2)}°F`);
+
+    console.log(a);
 }()); // Main IIFE
 
